08278ec4d4e3be912669ec6b38cf55fab5c43cc4
ceezess <nugentmayson@gmail.com>
2025-08-13 20:55:11 -0500

    hi

---
+++ main/java/mineshuko/tensei/MineshukoTensei.java
@@ -0,0 +1,16 @@
+package mineshuko.tensei;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MineshukoTensei implements ModInitializer {
+	public static final String MOD_ID = "mineshuko-tensei";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ main/java/mineshuko/tensei/MineshukoTenseiClient.java
@@ -0,0 +1,10 @@
+package mineshuko.tensei;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MineshukoTenseiClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
